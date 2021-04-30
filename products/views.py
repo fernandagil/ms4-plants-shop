0c214aa709c2aed939d383dbc478e9d3e7c32ee8
fernandagil <fernandagilgonzalez@gmail.com>
2021-04-30 21:27:16 +0000

    Create view to add product

--- products/views.py
+++ products/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .forms import ProductForm
+
+
+def add_product(request):
+    """ Add a product to the store """
+    form = ProductForm()
+    template = 'products/add_product.html'
+    context = {
+        'form': form,
+    }
+
+    return render(request, template, context)
