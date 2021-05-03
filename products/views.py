d4ef1fe7a4079f14180e97e5743b9e98ac50c0dd
fernandagil <fernandagilgonzalez@gmail.com>
2021-05-03 15:17:03 +0000

    Create all_products view and add urls

--- products/views.py
+++ products/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
-
+from .models import Product
 from .forms import ProductForm
+
+
+def all_products(request):
+    """ A view to show all products, including sorting and search queries """
+
+    products = Product.objects.all()
+
+    context = {
+        'products': products,
+    }
+
+    return render(request, 'products/products.html', context)
 
 
 def add_product(request):
