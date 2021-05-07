4f11a29501c8d2f80126c70a174c7a0e29550ac2
fernandagil <fernandagilgonzalez@gmail.com>
2021-05-07 21:36:38 +0000

    Update checkout template

--- plants_shop/settings.py
+++ plants_shop/settings.py
@@ -48,7 +48,7 @@ INSTALLED_APPS = [
     'checkout',
 
     # Other
-    'crispy-forms',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -79,6 +79,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',  # required by allauth
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
                 'bag.contexts.bag_contents',
             ],
             'builtins': [
