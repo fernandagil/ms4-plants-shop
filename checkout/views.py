500773128d6cdfe1fa298681fd6dad0e1fcd482b
fernandagil <fernandagilgonzalez@gmail.com>
2021-05-08 16:44:40 +0000

    Create payment intent

--- checkout/views.py
+++ checkout/views.py
@@ -25,13 +25,18 @@ def checkout(request):
         amount=stripe_total,
         currency=settings.STRIPE_CURRENCY,
     )
-    
+
     order_form = OrderForm()
+
+    if not stripe_public_key:
+        messages.warning(request, 'Stripe public key is missing. \
+            Did you forget to set it in your environment?')
+
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
-        'stripe_public_key': 'pk_test_51IfWGGGVilaIyzSqXzA2dNXlFldvAABVDapoe2JtnRB51aocspHFmpotUbtLejR2NoRmoFp4mZx6tWU65cwz4SId00Ovm0GKMm',
-        'client_secret': 'test client secret',
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret,
     }
 
     return render(request, template, context)
