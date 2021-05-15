82e74de306ab08e28e94a02d359c90fc9e357152
fernandagil <fernandagilgonzalez@gmail.com>
2021-05-15 13:39:19 +0000

    Customise Admin in blog app

--- blog/admin.py
+++ blog/admin.py
@@ -2,4 +2,15 @@ from django.contrib import admin
 from .models import Blog
 
 
-admin.site.register(Blog)
+class BlogAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+        'text',
+        'author',
+        'date_created',
+        'main_image'
+    )
+
+    ordering = ['-date_created']
+
+admin.site.register(Blog, BlogAdmin)
