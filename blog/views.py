bdb228dae8c51a49687a2574ccf28efc09e8b7cc
fernandagil <fernandagilgonzalez@gmail.com>
2021-05-15 13:44:18 +0000

    Create blog view and urls

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Blog
 
-# Create your views here.
+
+def all_blog(request):
+    """ A view to show all blog articles """
+
+    blog = Blog.objects.all()
+
+    context = {
+        'blog': blog,
+    }
+
+    return render(request, 'blog/blog.html', context)
