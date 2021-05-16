1d9c50e18700151234ebce590d4b73c56f384890
fernandagil <fernandagilgonzalez@gmail.com>
2021-05-16 14:18:33 +0000

    Create edit_post template, urls and link in blog pageg

--- blog/urls.py
+++ blog/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.all_blog, name='blog'),
     path('<int:blog_id>/', views.blog_post, name='blog_post'),
     path('add/', views.add_post, name='add_post'),
+    path('edit/<int:blog_id>/', views.edit_post, name='edit_post'),
 ]
