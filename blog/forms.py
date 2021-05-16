238378a7b396468e973ea3c266fd75842ff3fc29
fernandagil <fernandagilgonzalez@gmail.com>
2021-05-16 17:11:56 +0000

    Add widget to fix the image field

--- blog/forms.py
+++ blog/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from .widgets import CustomClearableFileInput
 from .models import Blog
 
 
@@ -6,3 +7,6 @@ class BlogForm(forms.ModelForm):
     class Meta:
         model = Blog
         fields = '__all__'
+
+    image = forms.ImageField(label='Image', required=False,
+                             widget=CustomClearableFileInput)
