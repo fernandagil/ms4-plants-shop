f85cf86e2f52bc6637acd940d10650486bbfc0b5
fernandagil <fernandagilgonzalez@gmail.com>
2021-05-20 10:44:53 +0000

    Update wishlist view

--- wishlist/views.py
+++ wishlist/views.py
@@ -1,13 +1,34 @@
 from django.shortcuts import render, redirect, reverse, HttpResponse, get_list_or_404, get_object_or_404
+from .models import Product, UserProfile, Wishlist, WishlistItem
 from django.contrib.auth.decorators import login_required
 from django.utils import timezone
 from django.contrib import messages
 
 
 def wishlist(request):
-    """ A view that renders the wishlist page """
+    """ A view to render the wishlist template """
+    items = []
+    user = get_object_or_404(UserProfile, user=request.user)
+    wishlist = Wishlist.objects.get_or_create(user=user)
+    wishlist_user = wishlist[0]
+    test = WishlistItem.objects.filter(wishlist=wishlist_user).exists()
 
-    return render(request, 'wishlist/wishlist.html')
+    if test:
+        user_wishlist = get_list_or_404(WishlistItem, wishlist=wishlist_user)
+        for obj in user_wishlist:
+            product = get_object_or_404(Product, name=obj)
+            items.append(product)
+        context = {
+            'wishlist': True,
+            'products': items
+        }
+        return render(request, 'wishlist/wishlist.html', context)
+
+    else:
+        context = {
+            'wishlist': False,
+        }
+        return render(request, 'wishlist/wishlist.html', context)
 
 
 def add_to_wishlist(request, product_id):
