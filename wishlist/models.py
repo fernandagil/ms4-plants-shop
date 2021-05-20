b383b2efd61e46e47a051fb907312fbf7c4feca3
fernandagil <fernandagilgonzalez@gmail.com>
2021-05-20 10:37:22 +0000

    Add models to wishlist app

--- wishlist/models.py
+++ wishlist/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from profiles.models import UserProfile
+from products.models import Product
 
-# Create your models here.
+
+class Wishlist(models.Model):
+    """ A model to link a bookmarked product to a user """
+    user = models.OneToOneField(UserProfile, null=False,
+                                blank=False,
+                                on_delete=models.CASCADE,
+                                related_name='wishlist')
+
+    def __str__(self):
+        return f'Wishlist ({self.user})'
+
+
+class WishlistItem(models.Model):
+    """A many to many model to bookmark products"""
+    wishlist = models.ForeignKey(Wishlist, null=False, blank=False,
+                                 on_delete=models.CASCADE,
+                                 related_name='wishlist_items')
+    product = models.ForeignKey(Product, null=False, blank=False,
+                                on_delete=models.CASCADE,
+                                related_name='wishlist_products')
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.product.name
