8b584fe35af58eb45d3050dd3ff083d68af8ab50
fernandagil <fernandagilgonzalez@gmail.com>
2021-05-22 14:35:36 +0000

    Remove secret key and set debug

--- plants_shop/settings.py
+++ plants_shop/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-q(epzp(7d_v)$hn)-pv7os)lm@&ofw__7@v0@9^8lumax1cj2z'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['ms4-plants-shop.herokuapp.com', 'localhost']
 
